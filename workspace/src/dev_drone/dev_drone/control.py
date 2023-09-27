0bebf04ab2053928f003e8d18c2fad2a225f752a
Adrien <adrien.pouxviel@gmail.com>
2023-09-27 12:17:01 +0200

    feat: wip add controls flip up and down

--- workspace/src/dev_drone/dev_drone/control.py
+++ workspace/src/dev_drone/dev_drone/control.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import rclpy
+import time
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, String
+from geometry_msgs.msg import Twist, Vector3
 
 class Control(Node):
     def __init__(self):
@@ -15,11 +17,48 @@ class Control(Node):
             10)
         self.subscription  # prevent unused variable warning
 
+        self.inAir = False
+
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.buttons)
-        if msg.buttons[0] == 1:
+        if msg.buttons[3] == 1 and not self.inAir:
             self.publisher_ = self.create_publisher(Empty, 'takeoff', 10)
-            self.publisher_.publish(msg)
+            self.publisher_.publish(Empty())
+            self.inAir = not self.inAir
+        elif msg.buttons[1] == 1 and self.inAir:
+            self.publisher_ = self.create_publisher(Empty, 'emergency', 10)
+            self.publisher_.publish(Empty())
+            self.inAir = not self.inAir
+        elif msg.buttons[0] == 1 and self.inAir:
+            self.publisher_ = self.create_publisher(Empty, 'land', 10)
+            self.publisher_.publish(Empty())
+            self.inAir = not self.inAir
+
+        elif msg.axes[7] == 1 and self.inAir:
+            self.publisher_ = self.create_publisher(String, 'flip', 10)
+            self.publisher_.publish(String(data = 'f'))
+            time.sleep(1)
+        elif msg.axes[7] == -1 and self.inAir:
+            self.publisher_ = self.create_publisher(String, 'flip', 10)
+            self.publisher_.publish(String(data = 'b'))
+            time.sleep(1)
+        elif msg.axes[6] == 1 and self.inAir:
+            self.publisher_ = self.create_publisher(String, 'flip', 10)
+            self.publisher_.publish(String(data = 'l'))
+            time.sleep(1)
+        elif msg.axes[6] == -1 and self.inAir:
+            self.publisher_ = self.create_publisher(String, 'flip', 10)
+            self.publisher_.publish(String(data = 'r'))
+            time.sleep(1)
+
+        elif msg.buttons[4] == 1:
+            self.publisher_ = self.create_publisher(Twist, 'control', 10)
+            self.publisher_.publish(Twist(linear = Vector3(x = 0.0, y = 0.0, z = -20.0), angular = Vector3(x = 0.0, y = 0.0, z = 0.0)))
+            time.sleep(1)
+        elif msg.buttons[5] == 1:
+            self.publisher_ = self.create_publisher(Twist, 'control', 10)
+            self.publisher_.publish(Twist(linear = Vector3(x = 0.0, y = 0.0, z = 20.0), angular = Vector3(x = 0.0, y = 0.0, z = 0.0)))
+            time.sleep(1)
 
 
 def main(args=None):
